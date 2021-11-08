5d701b994d22aa25969f4a48a0c235364228b731
George <80219357+GeorgeWhittington@users.noreply.github.com>
2021-11-08 13:55:51 +0000

    Add authentication checking to admin pages

--- hotel_website/admin.py
+++ hotel_website/admin.py
@@ -1,14 +1,26 @@
-from flask import Blueprint
+from flask import redirect, url_for, request
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+import flask_login
 
 from .models import db, User, Location, Currency, Hotel, Roomtype, Room, Booking
 
 admin = Admin(template_mode="bootstrap3")
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Location, db.session))
-admin.add_view(ModelView(Currency, db.session))
-admin.add_view(ModelView(Hotel, db.session))
-admin.add_view(ModelView(Roomtype, db.session))
-admin.add_view(ModelView(Room, db.session))
-admin.add_view(ModelView(Booking, db.session))
+
+
+class CustomModelView(ModelView):
+    def is_accessible(self):
+        print(f"{flask_login.current_user.is_authenticated} {type(flask_login.current_user)}")
+        return flask_login.current_user.is_authenticated and flask_login.current_user.admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("auth.login", next=request.url))
+
+
+admin.add_view(CustomModelView(User, db.session))
+admin.add_view(CustomModelView(Location, db.session))
+admin.add_view(CustomModelView(Currency, db.session))
+admin.add_view(CustomModelView(Hotel, db.session))
+admin.add_view(CustomModelView(Roomtype, db.session))
+admin.add_view(CustomModelView(Room, db.session))
+admin.add_view(CustomModelView(Booking, db.session))
